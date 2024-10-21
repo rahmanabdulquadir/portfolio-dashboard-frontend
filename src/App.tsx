0e849c694c68c0cc1376f5158880e36cf51a4296
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-21 17:35:10 +0600

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Welcome to the portfolio dashboard</h1>
+     <Button>Dashboard</Button>
+    </>
+  )
+}
+
+export default App
