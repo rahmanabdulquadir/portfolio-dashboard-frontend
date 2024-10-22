3c51517da32cfef6a73940366f4bde8840802876
rahmanabdulquadir <rahmanaq777@gmail.com>
2024-10-22 10:36:56 +0600

    file structure organized

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,14 @@
-import { Calendar, Home, Inbox, Magnet, Search, Settings, Text } from "lucide-react";
-import "./App.css";
-import { Button } from "./components/ui/button";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "./components/ui/sidebar";
 import Dashboard from "./pages/Dashboard";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const items = [
-    {
-      title: "Projects",
-      url: "#",
-      icon: Home,
-    },
-    {
-      title: "Blog",
-      url: "#",
-      icon: Text,
-    },
-    {
-      title: "Skill",
-      url: "#",
-      icon: Magnet,
-    },
-  ];
+ 
 
   return (
     <>
     <Dashboard/>
     <Outlet/>
-      {/* <Sidebar>
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <a href={item.url}>
-                        <item.icon />
-                        <h3 className="text-lg">{item.title}</h3>
-                      </a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </Sidebar> */}
+      
     </>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,55 @@
-
+import { Home, Magnet, Text } from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../components/ui/sidebar";
 
 const Dashboard = () => {
+  const items = [
+    {
+      title: "Projects",
+      url: "#",
+      icon: Home,
+    },
+    {
+      title: "Blog",
+      url: "#",
+      icon: Text,
+    },
+    {
+      title: "Skill",
+      url: "#",
+      icon: Magnet,
+    },
+  ];
   return (
-    <div>
-      This is dashboard
-    </div>
+    <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <h3 className="text-lg">{item.title}</h3>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
   );
 };
 
